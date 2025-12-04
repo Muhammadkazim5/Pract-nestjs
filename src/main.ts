eba4d4eab26dfac1ffea52df98f3677aeef838c5
muhammadkazim5 <muhammadkazim5806@gmail.com>
2025-12-04 11:19:11 +0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
@@ -9,6 +9,15 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
+  const config = new DocumentBuilder()
+    .setTitle('Blog API example')
+    .setDescription('The blog API description')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
