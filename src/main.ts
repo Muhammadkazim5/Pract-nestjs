cdbdb17c7a949d529ed976e62fd0b367b7612d7b
muhammadkazim5 <muhammadkazim5806@gmail.com>
2025-12-10 16:49:06 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/public/', // optional URL prefix
+  });
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
